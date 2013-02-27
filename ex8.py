16244e45d417ca336e02671f335e47608d0427cb
Susan Powell <susan.powell@yale.edu>
2013-02-27 10:43:58 -0500

    more lessons

---
+++ ex8.py
@@ -0,0 +1,14 @@
+# %r is used for debugging (and returns strings with quotes, either single or double)
+# %s is for normal use
+formatter = "%r : %r : %s : %s"
+
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+	"I had this thing.",
+	"That you could type up right.",
+	"But it didn't sing.",
+	"So I said goodnight."
+)
